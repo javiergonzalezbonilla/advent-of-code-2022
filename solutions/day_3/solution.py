95aadf2c5e1a9c7892a2ee876eaf63a32c118f57
javiergonzalezbonilla <javier.gonzalezbonilla@getrev.ai>
2022-12-04 22:43:56 -0600

    day3 part 2

--- solutions/day_3/solution.py
+++ solutions/day_3/solution.py
@@ -1,5 +1,6 @@
 import os
 from solutions import utils
+from functools import reduce
 
 def solution(rucksacks):
     priorities_cum_sum = 0
@@ -8,15 +9,30 @@ def solution(rucksacks):
         compartment_1 = set(rucksack[:compartment_length])
         compartment_2 = set(rucksack[compartment_length:])
         common = compartment_1.intersection(compartment_2).pop()
-        if common.islower():
-            priorities_cum_sum += ord(common) - 96
-        else:
-            priorities_cum_sum += ord(common) - 65 + 27
+        priorities_cum_sum += find_priority( common)
     return priorities_cum_sum
+
+def solution_2(rucksacks):
+    rucksacks_triplets_count = len(rucksacks) // 3
+    priorities_cum_sum = 0
+    for triplet_index in range(rucksacks_triplets_count):
+        triplet = rucksacks[triplet_index*3: (triplet_index + 1) * 3]
+        common = reduce(lambda x,y: set(x) & set(y), triplet)
+        priorities_cum_sum += find_priority(common.pop())
+    return priorities_cum_sum
+
+
+def find_priority(common):
+    if common.islower():
+        return ord(common) - 96
+    else:
+        return ord(common) - 65 + 27
 
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     rucksacks = utils.ReadFile(dir_path + "/input.txt").get_data_from_line()
     value = solution(rucksacks)
-    print(f'Total Value {value}')
+    print(f'Total Value: solution 1: {value}')
+    value = solution_2(rucksacks)
+    print(f'Total Value: solution 2: {value}')
 
